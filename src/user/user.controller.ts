648298c65bbb47e8866115a16512edf7a9984c06
lemoye622 <2557418823@qq.com>
2024-05-13 10:12:33 +0800

    feat: 新增用户注册接口

---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  async register(@Body() registerUserDto: RegisterUserDto) {
+    return await this.userService.register(registerUserDto);
+  }
+}
