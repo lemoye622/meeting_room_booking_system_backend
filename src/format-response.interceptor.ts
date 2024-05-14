902def3775c3b8ab9dbf7816f6c75d806c2f9d26
lemoye622 <2557418823@qq.com>
2024-05-14 16:13:16 +0800

    feat: 新增修改响应内容格式的拦截器

---
+++ src/format-response.interceptor.ts
@@ -0,0 +1,25 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class FormatResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    
+    const res = context.switchToHttp().getResponse();
+
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          code: res.statusCode,
+          message: 'success',
+          data,
+        };
+      }),
+    );
+  }
+}
