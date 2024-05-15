8500dfff3a9478aa534b29684ce40d2b29d542cc
lemoye622 <2557418823@qq.com>
2024-05-15 10:41:57 +0800

    feat: 新增修改个人信息时发送验证码的接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -116,9 +116,7 @@ export class UserController {
   @Get('update_password/captcha')
   async updatePasswordCaptcha(@Query('address') address: string) {
     const code = Math.random().toString().slice(2,8);
-
     await this.redisService.set(`update_password_captcha_${address}`, code, 10 * 60);
-
     await this.emailService.sendMail({
       to: address,
       subject: '更改密码验证码',
@@ -131,5 +129,17 @@ export class UserController {
   @RequireLogin()
   async updateUserInfo(@UserInfo('userId') userId: number, @Body() updateUserDto: UpdateUserDto) {
     return await this.userService.updateUserInfo(userId, updateUserDto);
+  }
+
+  @Get('update/captcha')
+  async updateCaptcha(@Query('address') address: string) {
+    const code = Math.random().toString().slice(2,8);
+    await this.redisService.set(`update_user_captcha_${address}`, code, 10 * 60);
+    await this.emailService.sendMail({
+      to: address,
+      subject: '更改用户信息验证码',
+      html: `<p>你的验证码是 ${code}</p>`
+    });
+    return '发送成功';
   }
 }
