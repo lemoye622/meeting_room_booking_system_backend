719cb654a63b2b522a8ec0e5539f47c479c0d8e8
lemoye622 <2557418823@qq.com>
2024-05-17 11:24:37 +0800

    feat: 支持跨域

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
   app.useGlobalInterceptors(new InvokeRecordInterceptor());
   app.useGlobalFilters(new CustomExceptionFilter());
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('会议室预订系统')
     .setDescription('api 接口文档')
